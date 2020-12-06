789d903a9e717c245177a6ccf0f7cd229ce87557
99003163 <Shreya.kori@ltts.com>
2020-12-06 11:53:13 +0530

    new

--- Miniproject_C/3_implementation/src/calculator_operations.c
+++ Miniproject_C/3_implementation/src/calculator_operations.c
@@ -93,11 +93,12 @@ int factorial(int number)
     return -1;
 
   /* Return 1 for 0 */
-  if(number == 0)
+  else if(number == 0)
     return 1;
 
   /* Recursively calculate Factorial of the number */
-  return number * factorial(number-1);
+  else
+    return number * factorial(number-1);
 }
 
 int modulus(int operand1, int operand2)
